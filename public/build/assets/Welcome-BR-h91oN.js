51f7693c8fdcb979ccaae7df803323a464c5df44
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2024-10-17 17:58:14 +0100

    Handled Chart data

--- public/build/assets/Welcome-BR-h91oN.js
+++
@@ -1,1 +0,0 @@
-import{j as t,Y as e}from"./app-DDrCQHcv.js";import{D as o}from"./AuthenticatedLayout-he-aaQrG.js";import"./Disclose-oIoCyYJZ.js";function s(){return t.jsx(o,{title:"Health Practitioners",children:t.jsx(e,{title:"Welcome"})})}export{s as default};
--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -2,7 +2,7 @@ import { Link } from '@inertiajs/react';
 
 export default function Guest({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-white pt-6 sm:justify-center sm:pt-0">
+        <div className="flex min-h-screen flex-col items-center bg-white pt-6 justify-center px-2 sm:pt-0">
             {children}
         </div>
     );
