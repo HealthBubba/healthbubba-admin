a944fac11fcddd1b7b6cba8acfc1bb23d22b3716
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2024-10-17 18:04:07 +0100

    Handled Chart data

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,9 +9,11 @@ use App\Enums\Role;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Builder;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
 use Illuminate\Notifications\Notifiable;
 
-class User extends Model {
+class User extends Authenticatable {
     use HasFactory, Notifiable, HasQuery;
 
     protected $fillable = [
