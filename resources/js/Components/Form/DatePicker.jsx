a43b39f5d0298b54fba7314bdb775dbf4b7b9dee
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2024-10-19 17:58:49 +0100

    Handled Date picker

--- resources/js/Components/Form/DatePicker.jsx
+++ resources/js/Components/Form/DatePicker.jsx
@@ -1,23 +1,44 @@
 import SettingIcon from '@/Icons/SettingIcon';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 import React, { useState } from 'react'
 import Datepicker from "react-tailwindcss-datepicker";
+import Disclose from '../Disclose';
 
-export default ({value = { startDate: null, endDate: null }, onChange}) => {
+const defaultValue = { 
+    startDate: null, 
+    endDate: null 
+}
+
+export default ({val = { startDate: null, endDate: null }, onChange: isChanged = null}) => {
+
+    const [value, setValue] = useState(val)
+    const onChange = (value) => {
+        setValue(value)
+        if(isChanged) isChanged(value)
+    }
 
     return (
-        <label className="border-2 p-1 h-full px-3 font-medium items-center space-x-1 text-sm rounded-lg inline-flex">
-            <SettingIcon className='size-5' />         
-            <Datepicker 
-                value={value}
-                primaryColor={"green"}
-                toggleClassName="hidden"
-                inputClassName="border-0 bg-transparent focus:ring-0 focus:border-0 focus:outline-0 py-1 px-0 text-sm"
-                containerClassName="relative"
-                placeholder='Date'
-                onChange={onChange}
-                showShortcuts={true}
-            />
-        </label>
+        <div className='border-2 h-full font-medium items-center space-x-1 text-sm rounded-lg inline-flex'>
+            <label className="inline-flex px-3 p-1 cursor-pointer items-center space-x-1">
+                <SettingIcon className='size-5' />         
+                <Datepicker 
+                    value={value}
+                    primaryColor={"green"}
+                    toggleClassName="hidden"
+                    inputClassName="border-0 bg-transparent cursor-pointer focus:ring-0 focus:border-0 focus:outline-0 py-1 px-0 text-sm"
+                    containerClassName="relative"
+                    readOnly
+                    placeholder='Date'
+                    onChange={onChange}
+                    showShortcuts={true}
+                />
+            </label>
+            <Disclose show={!!value.startDate && !!value.endDate} >
+                <button  onClick={() => setValue(defaultValue)} className="p-0 px-2">
+                    <XMarkIcon className='size-5' />
+                </button>
+            </Disclose>
+        </div>
 
     )
 }
