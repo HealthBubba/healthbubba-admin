9e2cd9b0c58add4ec01148bdc2cf141c31d702c7
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2024-12-24 15:50:12 +0100

    Adhsdhsd message

--- app/Http/Controllers/HealthPractitionerController.php
+++ app/Http/Controllers/HealthPractitionerController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\HtmlString;
 use Inertia\Inertia;
 
 class HealthPractitionerController extends Controller {
@@ -37,6 +38,19 @@ class HealthPractitionerController extends Controller {
         $user->save();
 
         // Dispatch approval message if neccessary
+        notify('Your Account is Now Verified')
+            ->greeting("Dear {$user->fullname}")
+            ->line('Congratulations! We are pleased to inform you that your account has been successfully verified on the Health Bubba platform.')
+            ->line("As a verified doctor, you can now:")
+            ->line("• Access Patient Consultations: Start accepting and managing appointments through our secure platform.")
+            ->line("• Set Your Availability: Define your schedule and consultation fees to suit your preferences.")
+            ->line("• Earn Flexibly: Participate in our pay-as-you-go model or opt for in-house positions based on your consultations and reviews.")
+            ->line("• Utilize Advanced Tools: Leverage our integrated features, including telemedicine, emergency flow connections, and more, to provide top-notch care to patients.")
+            ->line('We are excited to have you as part of our mission to make healthcare accessible, efficient, and patient-centric.')
+            ->line(new HtmlString("If you have any questions or need assistance, feel free to reach out to our support team at <a href='mailto:support@healthbubba.com'>support@healthbubba.com</a>."))
+            ->line('Thank you for joining Health Bubba. Together, let’s transform healthcare delivery!')
+            ->salutation('Warm regards, The Health Bubba Team')
+            ->sendNow($user, ['mail']);
 
         toast('Health practitioner account verified successfully!')->success();
         return back();
