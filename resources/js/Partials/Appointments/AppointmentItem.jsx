259a0c12eb4c9fa2c66c3cc241702ac46b781c71
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2025-07-07 21:47:06 +0100

    Updates for appointements

--- resources/js/Partials/Appointments/AppointmentItem.jsx
+++ resources/js/Partials/Appointments/AppointmentItem.jsx
@@ -1,6 +1,7 @@
 import { Badge } from '@/Components/Badge'
 import Currency from '@/Components/Currency'
 import TableLink from '@/Components/Table/TableLink'
+import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/24/solid'
 import { Link } from '@inertiajs/react'
 import React from 'react'
 
@@ -26,6 +27,7 @@ export default function AppointmentItem({appointment}) {
                     <Link href={route('practitioners.show', {user: appointment.doctor?.id})} className='link' >{appointment.doctor?.full_name}</Link>
                 }
             </td>
+            <TableLink className='text-center' href={route('appointments.show', {appointment: appointment.id})}>{appointment.is_physical ? <CheckCircleIcon className='text-primary size-6' /> : <XCircleIcon className='text-red-600 size-6' /> }</TableLink>
             <TableLink href={route('appointments.show', {appointment: appointment.id})}>{appointment.status ? 'Completed' : 'Pending'}</TableLink>
             <TableLink href={route('appointments.show', {appointment: appointment.id})}>{appointment.date}</TableLink>
             <TableLink href={route('appointments.show', {appointment: appointment.id})}>
